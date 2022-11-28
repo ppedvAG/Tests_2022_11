2e53a43cc3c50d87593d8e4f6e26c1464cbe60ae
André <AndreR@ppedv.de>
2022-11-28 15:53:48 +0100

    OpeningHours implementierung

--- TddBank/TddBank/OpeningHours.cs
+++ TddBank/TddBank/OpeningHours.cs
@@ -2,9 +2,21 @@
 {
     public class OpeningHours
     {
-        public bool IsOpen(DateTime dateAndTime)
+        public bool IsOpen(DateTime dt)
         {
-            throw new NotImplementedException();
+            var start = new TimeSpan(10, 30, 0);
+            var ende = new TimeSpan(19, 00, 0);
+            var endeSa = new TimeSpan(14, 00, 0);
+
+
+            //häßlich aber geht
+            if (dt.DayOfWeek == DayOfWeek.Sunday) return false;
+            else if (dt.DayOfWeek == DayOfWeek.Saturday && dt.TimeOfDay >= start && dt.TimeOfDay < endeSa)
+                return true;
+            else if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday && dt.TimeOfDay >= start && dt.TimeOfDay < ende)
+                return true;
+
+            return false;
         }
     }
 }
