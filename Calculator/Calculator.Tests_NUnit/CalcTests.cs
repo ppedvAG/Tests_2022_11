64d46e64b940123510f84a8e845db8d3a081c7b3
André <AndreR@ppedv.de>
2022-11-28 13:47:47 +0100

    Calctests mit NUnit

---
+++ Calculator/Calculator.Tests_NUnit/CalcTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests_NUnit
+{
+    [TestFixture]
+    public class CalcTests
+    {
+        [Test]
+        public void Sum_4_and_6_results_10()
+        {
+            var calc = new Calc();
+
+            var result = calc.Sum(4, 6);
+
+            //Assert.AreEqual(10, result);
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(2, 3, 5)]
+        [TestCase(-2, -3, -5)]
+        [TestCase(-2, 3, 1)]
+        public void Sum_ok(int a, int b, int exp)
+        {
+            var calc = new Calc();
+
+            var result = calc.Sum(a, b);
+
+            Assert.That(result, Is.EqualTo(exp));
+        }
+
+        [Test]
+        public void Sum_MIN_and_n1_throws_OverflowException()
+        {
+            var calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MinValue, -1));
+        }
+    }
+}
